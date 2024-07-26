f950561400f9daca7a46ec28b4e9bb51d087b69e
AliGhadirii <ali.ghadiri78@gmail.com>
2024-07-26 19:59:11 +1000

    metrics for eye dataset added

--- Utils/Metrics.py
+++ Utils/Metrics.py
@@ -12,12 +12,7 @@ import matplotlib.pyplot as plt
 
 
 def cal_metrics_skin(df):
-    """
-    calculate average accuracy, accuracy per skin type, PQD, DPM, EOM, EOpp0, EOpp1, EOdd, and NAR.
-    Skin type in the input df should be in the range of [0,5].
-    input val results csv path, type_indices: a list
-    output a dic, 'acc_avg': value, 'acc_per_type': array[x,x,x], 'PQD', 'DPM', 'EOM'
-    """
+
     is_binaryCLF = len(df["label"].unique()) == 2
     num_classes = len(df["label"].unique())
 
@@ -361,57 +356,61 @@ def cal_metrics_skin(df):
 
 
 def cal_metrics_eye(df, SA_level):
-    is_binaryCLF = len(df["label"].unique()) == 2
+
     num_classes = len(df["label"].unique())
+    is_binaryCLF = len(df["label"].unique()) == 2
 
     type_indices = sorted(list(df[SA_level].unique()))
+    is_binarySA = len(type_indices) == 2
+    if not is_binarySA:
+        SA_level_binary = SA_level.replace("binary", "multi")
 
     labels_array = np.zeros((len(type_indices), len(df["label"].unique())))
     correct_array = np.zeros((len(type_indices), len(df["label"].unique())))
     predictions_array = np.zeros((len(type_indices), len(df["label"].unique())))
     prob_array = [[] for i in range(len(type_indices))]
-    label_array_per_fitz = [[] for i in range(len(type_indices))]
+    label_array_per_SA = [[] for i in range(len(type_indices))]
 
-    labels_array_binary = np.zeros((2, len(df["label"].unique())))
-    correct_array_binary = np.zeros((2, len(df["label"].unique())))
-    predictions_array_binary = np.zeros((2, len(df["label"].unique())))
-
-    positive_list = []  # get positive probability for binary classification
-    labels_ft0 = []
-    labels_ft1 = []
-    predictions_ft0 = []
-    predictions_ft1 = []
+    positive_list = []
+    labels_SA0 = []
+    labels_SA1 = []
+    predictions_SA0 = []
+    predictions_SA1 = []
 
     for i in range(df.shape[0]):
         prediction = df.iloc[i]["prediction"]
         label = df.iloc[i]["label"]
-        type = df.iloc[i]["fitzpatrick"]
-        type_binary = df.iloc[i]["fitzpatrick_binary"]
+        type = df.iloc[i][SA_level]
+        if not is_binarySA:
+            type_binary = df.iloc[i][SA_level_binary]
 
         labels_array[int(type), int(label)] += 1
         predictions_array[int(type), int(prediction)] += 1
         if prediction == label:
             correct_array[int(type), int(label)] += 1
 
-        labels_array_binary[int(type_binary), int(label)] += 1
-        predictions_array_binary[int(type_binary), int(prediction)] += 1
-        if prediction == label:
-            correct_array_binary[int(type_binary), int(label)] += 1
-
         if is_binaryCLF:
             prob_array[int(type)].append(df.iloc[i]["prediction_probability"])
-            label_array_per_fitz[int(type)].append(label)
+            label_array_per_SA[int(type)].append(label)
             if prediction == 0:
                 positive_list.append(1.0 - df.iloc[i]["prediction_probability"])
             else:
                 positive_list.append(df.iloc[i]["prediction_probability"])
 
-        if type_binary == 0:
-            labels_ft0.append(label)
-            predictions_ft0.append(prediction)
+        if is_binarySA:
+            if type == 0:
+                labels_SA0.append(label)
+                predictions_SA0.append(prediction)
+            else:
+                labels_SA1.append(label)
+                predictions_SA1.append(prediction)
         else:
-            labels_ft1.append(label)
-            predictions_ft1.append(prediction)
+            if type_binary == 0:
+                labels_SA0.append(label)
+                predictions_SA0.append(prediction)
+            else:
+                labels_SA1.append(label)
+                predictions_SA1.append(prediction)
 
     correct_array = correct_array[type_indices]
     labels_array = labels_array[type_indices]
@@ -424,8 +423,8 @@ def cal_metrics_eye(df, SA_level):
     for i in range(len(type_indices)):
         acc_array.append(
             accuracy_score(
-                df[df["fitzpatrick"] == i]["label"],
-                df[df["fitzpatrick"] == i]["prediction"],
+                df[df[SA_level] == i]["label"],
+                df[df[SA_level] == i]["prediction"],
             )
             * 100
         )
@@ -438,8 +437,8 @@ def cal_metrics_eye(df, SA_level):
     for i in range(len(type_indices)):
         F1_W_array.append(
             f1_score(
-                df[df["fitzpatrick"] == i]["label"],
-                df[df["fitzpatrick"] == i]["prediction"],
+                df[df[SA_level] == i]["label"],
+                df[df[SA_level] == i]["prediction"],
                 average="weighted",
             )
             * 100
@@ -453,8 +452,8 @@ def cal_metrics_eye(df, SA_level):
     for i in range(len(type_indices)):
         F1_Mac_array.append(
             f1_score(
-                df[df["fitzpatrick"] == i]["label"],
-                df[df["fitzpatrick"] == i]["prediction"],
+                df[df[SA_level] == i]["label"],
+                df[df[SA_level] == i]["prediction"],
                 average="macro",
             )
             * 100
@@ -486,10 +485,10 @@ def cal_metrics_eye(df, SA_level):
         # AUC per skin type
         AUC = roc_auc_score(df["label"], df["prediction_probability"]) * 100
         AUC_per_type = []
-        for i in range(len(label_array_per_fitz)):
+        for i in range(len(label_array_per_SA)):
             try:
                 AUC_per_type.append(
-                    roc_auc_score(label_array_per_fitz[i], prob_array[i]) * 100
+                    roc_auc_score(label_array_per_SA[i], prob_array[i]) * 100
                 )
             except:
                 AUC_per_type.append(np.nan)
@@ -502,8 +501,8 @@ def cal_metrics_eye(df, SA_level):
         )
 
         AUC_per_type = []
-        for i in range(len(label_array_per_fitz)):
-            df_filtered = df[df["fitzpatrick"] == i]
+        for i in range(len(label_array_per_SA)):
+            df_filtered = df[df[SA_level] == i]
             all_probs = np.array(df_filtered["all_probability"].to_list())
             try:
                 AUC_per_type.append(
@@ -521,150 +520,120 @@ def cal_metrics_eye(df, SA_level):
 
     ##############################          Metrics with binary Sensative attribute         ##############################
 
-    correct_array_binary = correct_array_binary[type_indices_binary]
-    labels_array_binary = labels_array_binary[type_indices_binary]
-    predictions_array_binary = predictions_array_binary[type_indices_binary]
-
-    # avg acc, acc per type
-    correct_array_sumc_binary, labels_array_sumc_binary = np.sum(
-        correct_array_binary, axis=1
-    ), np.sum(
-        labels_array_binary, axis=1
-    )  # sum skin conditions
-    acc_array_binary = correct_array_sumc_binary / labels_array_sumc_binary
-    avg_acc_binary = (np.sum(correct_array_binary) / np.sum(labels_array_binary)) * 100
-
-    # PQD
-    PQD_binary = acc_array_binary.min() / acc_array_binary.max()
-
-    # DPM
-    demo_array_binary = predictions_array_binary / np.sum(
-        predictions_array_binary, axis=1, keepdims=True
-    )
-    DPM_binary = np.mean(demo_array_binary.min(axis=0) / demo_array_binary.max(axis=0))
-
-    # EOM
-    eo_array_binary = correct_array_binary / labels_array_binary
-    EOM_binary = np.mean(
-        np.nanmin(eo_array_binary, axis=0) / np.nanmax(eo_array_binary, axis=0)
-    )
-
-    # getting class-wise TPR, FPR, TNR for fitzpatrick 0
-    conf_matrix_fitz0 = confusion_matrix(labels_ft0, predictions_ft0)
+    # getting class-wise TPR, FPR, TNR for SA 0
+    conf_matrix_SA0 = confusion_matrix(labels_SA0, predictions_SA0)
 
     # Initialize lists to store TPR, TNR, FPR for each class
-    class_tpr_fitz0 = []
-    class_tnr_fitz0 = []
-    class_fpr_fitz0 = []
+    class_tpr_SA0 = []
+    class_tnr_SA0 = []
+    class_fpr_SA0 = []
 
-    for i in range(len(conf_matrix_fitz0)):
+    for i in range(len(conf_matrix_SA0)):
         # Calculate TPR for class i
-        tpr = conf_matrix_fitz0[i, i] / sum(conf_matrix_fitz0[i, :])
-        class_tpr_fitz0.append(tpr)
+        tpr = conf_matrix_SA0[i, i] / sum(conf_matrix_SA0[i, :])
+        class_tpr_SA0.append(tpr)
 
         # Calculate TNR for class i
         tn = (
-            sum(sum(conf_matrix_fitz0))
-            - sum(conf_matrix_fitz0[i, :])
-            - sum(conf_matrix_fitz0[:, i])
-            + conf_matrix_fitz0[i, i]
+            sum(sum(conf_matrix_SA0))
+            - sum(conf_matrix_SA0[i, :])
+            - sum(conf_matrix_SA0[:, i])
+            + conf_matrix_SA0[i, i]
         )
-        fp = sum(conf_matrix_fitz0[:, i]) - conf_matrix_fitz0[i, i]
-        fn = sum(conf_matrix_fitz0[i, :]) - conf_matrix_fitz0[i, i]
+        fp = sum(conf_matrix_SA0[:, i]) - conf_matrix_SA0[i, i]
+        fn = sum(conf_matrix_SA0[i, :]) - conf_matrix_SA0[i, i]
         tnr = tn / (tn + fp)
-        class_tnr_fitz0.append(tnr)
+        class_tnr_SA0.append(tnr)
 
         # Calculate FPR for class i
         fpr = 1 - tnr
-        class_fpr_fitz0.append(fpr)
+        class_fpr_SA0.append(fpr)
 
-    # getting class-wise TPR, FPR, TNR for fitzpatrick 1
+    # getting class-wise TPR, FPR, TNR for SA 1
 
-    conf_matrix_fitz1 = confusion_matrix(labels_ft1, predictions_ft1)
+    conf_matrix_SA1 = confusion_matrix(labels_SA1, predictions_SA1)
 
     # Check if there is any class that is not in both subgroups to handle it
     try:
-        class_idx = (
-            set(df[df["fitzpatrick_binary"] == 0]["label"].unique())
-            - set(df[df["fitzpatrick_binary"] == 1]["label"].unique())
-        ).pop()
-        conf_matrix_fitz1 = np.insert(conf_matrix_fitz1, class_idx, 0, axis=1)
-        conf_matrix_fitz1 = np.insert(conf_matrix_fitz1, class_idx, 0, axis=0)
+        if is_binarySA:
+            class_idx = (
+                set(df[df[SA_level] == 0]["label"].unique())
+                - set(df[df[SA_level] == 1]["label"].unique())
+            ).pop()
+        else:
+            class_idx = (
+                set(df[df[SA_level_binary] == 0]["label"].unique())
+                - set(df[df[SA_level_binary] == 1]["label"].unique())
+            ).pop()
+        conf_matrix_SA1 = np.insert(conf_matrix_SA1, class_idx, 0, axis=1)
+        conf_matrix_SA1 = np.insert(conf_matrix_SA1, class_idx, 0, axis=0)
         print(f"INFO: class {class_idx} is not in both binary subgroups")
     except:
         class_idx = None
 
     # Initialize lists to store TPR, TNR, FPR for each class
-    class_tpr_fitz1 = []
-    class_tnr_fitz1 = []
-    class_fpr_fitz1 = []
+    class_tpr_SA1 = []
+    class_tnr_SA1 = []
+    class_fpr_SA1 = []
 
-    for i in range(len(conf_matrix_fitz1)):
+    for i in range(len(conf_matrix_SA1)):
         # Calculate TPR for class i
-        tpr = conf_matrix_fitz1[i, i] / sum(conf_matrix_fitz1[i, :])
-        class_tpr_fitz1.append(tpr)
+        tpr = conf_matrix_SA1[i, i] / sum(conf_matrix_SA1[i, :])
+        class_tpr_SA1.append(tpr)
 
         # Calculate TNR for class i
         tn = (
-            sum(sum(conf_matrix_fitz1))
-            - sum(conf_matrix_fitz1[i, :])
-            - sum(conf_matrix_fitz1[:, i])
-            + conf_matrix_fitz1[i, i]
+            sum(sum(conf_matrix_SA1))
+            - sum(conf_matrix_SA1[i, :])
+            - sum(conf_matrix_SA1[:, i])
+            + conf_matrix_SA1[i, i]
         )
-        fp = sum(conf_matrix_fitz1[:, i]) - conf_matrix_fitz1[i, i]
-        fn = sum(conf_matrix_fitz1[i, :]) - conf_matrix_fitz1[i, i]
+        fp = sum(conf_matrix_SA1[:, i]) - conf_matrix_SA1[i, i]
+        fn = sum(conf_matrix_SA1[i, :]) - conf_matrix_SA1[i, i]
         tnr = tn / (tn + fp)
-        class_tnr_fitz1.append(tnr)
+        class_tnr_SA1.append(tnr)
 
         # Calculate FPR for class i
         fpr = 1 - tnr
-        class_fpr_fitz1.append(fpr)
+        class_fpr_SA1.append(fpr)
 
     if class_idx is not None:
-        class_tpr_fitz1[class_idx] = np.nan
-        class_tnr_fitz1[class_idx] = np.nan
-        class_fpr_fitz1[class_idx] = np.nan
+        class_tpr_SA1[class_idx] = np.nan
+        class_tnr_SA1[class_idx] = np.nan
+        class_fpr_SA1[class_idx] = np.nan
 
     # EOpp0
     EOpp0 = 0
-    for c in range(len(class_tnr_fitz0)):
-        val = abs(class_tnr_fitz1[c] - class_tnr_fitz0[c])
+    for c in range(len(class_tnr_SA0)):
+        val = abs(class_tnr_SA1[c] - class_tnr_SA0[c])
         if not np.isnan(val):
             EOpp0 += val
 
-    EOpp0_new = np.abs((np.array(class_tnr_fitz1) - np.array(class_tnr_fitz0))).mean()
+    EOpp0_new = np.abs((np.array(class_tnr_SA1) - np.array(class_tnr_SA0))).mean()
 
     # EOpp1
     EOpp1 = 0
-    for c in range(len(class_tpr_fitz0)):
-        val = abs(class_tpr_fitz1[c] - class_tpr_fitz0[c])
+    for c in range(len(class_tpr_SA0)):
+        val = abs(class_tpr_SA1[c] - class_tpr_SA0[c])
         if not np.isnan(val):
             EOpp1 += val
-    EOpp1_new = np.abs((np.array(class_tpr_fitz1) - np.array(class_tpr_fitz0))).mean()
+    EOpp1_new = np.abs((np.array(class_tpr_SA1) - np.array(class_tpr_SA0))).mean()
 
     # EOdd
     EOdd_new = (
         np.abs(
-            (np.array(class_tpr_fitz1) - np.array(class_tpr_fitz0))
-            + (np.array(class_fpr_fitz1) - np.array(class_fpr_fitz0))
+            (np.array(class_tpr_SA1) - np.array(class_tpr_SA0))
+            + (np.array(class_fpr_SA1) - np.array(class_fpr_SA0))
         ).mean()
         / 2
     )
     EOdd = 0
-    for c in range(len(class_tpr_fitz0)):
+    for c in range(len(class_tpr_SA0)):
         val = abs(
-            class_tpr_fitz1[c]
-            - class_tpr_fitz0[c]
-            + class_fpr_fitz1[c]
-            - class_fpr_fitz0[c]
+            class_tpr_SA1[c] - class_tpr_SA0[c] + class_fpr_SA1[c] - class_fpr_SA0[c]
         )
         if not np.isnan(val):
             EOdd += val
-
-    # NAR
-    NAR_binary = (
-        acc_array_binary.max() - acc_array_binary.min()
-    ) / acc_array_binary.mean()
 
     return {
         "accuracy": Accuracy,
@@ -693,12 +662,6 @@ def cal_metrics_eye(df, SA_level):
         "NAR": NAR,
         "NFR_W": NFR_W,
         "NFR_Mac": NFR_Mac,
-        "acc_avg_binary": avg_acc_binary,
-        "acc_per_type_binary": acc_array_binary,
-        "PQD_binary": PQD_binary,
-        "DPM_binary": DPM_binary,
-        "EOM_binary": EOM_binary,
-        "NAR_binary": NAR_binary,
     }
 
 
